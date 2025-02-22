63dbe83f3ab7dd2ffd84bff7251599cc3c35261b
Lucas Vilar <lucas.vilar.carneiro@gmail.com>
2025-02-22 17:25:27 -0300

    Title e location no modal de participação do torneio

--- client/src/components/Tournaments/JoinTournamentModal.tsx
+++ client/src/components/Tournaments/JoinTournamentModal.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
 import { UploadIcon } from "../icons/UploadIcon";
+import Image from "next/image";
+import { CloseIcon } from "../icons/CloseIcon";
 
 
 interface Photo {
@@ -17,6 +19,7 @@ interface JoinTournamentModalProps {
 
 export default function JoinTournamentModal({ onClose }: JoinTournamentModalProps) {
     const [uploadedPhoto, setUploadedPhoto] = useState<Photo | null>(null);
+    const [photoURL, setPhotoURL] = useState<string | null>(null);
     const [dragging, setDragging] = useState(false);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
@@ -58,6 +61,7 @@ export default function JoinTournamentModal({ onClose }: JoinTournamentModalProp
         };
 
         setUploadedPhoto(newPhoto);
+        setPhotoURL(URL.createObjectURL(imageFiles[0]))
     };
 
     const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -89,9 +93,10 @@ export default function JoinTournamentModal({ onClose }: JoinTournamentModalProp
         };
 
         setUploadedPhoto(newPhoto);
+        setPhotoURL(URL.createObjectURL(imageFiles[0]))
     };
 
-    const handlePhotoNameChange = (newName: string) => {
+    const handlePhotoTitleChange = (newName: string) => {
         setUploadedPhoto(prev => prev ? { ...prev, title: newName } : null);
     };
 
@@ -116,38 +121,92 @@ export default function JoinTournamentModal({ onClose }: JoinTournamentModalProp
         }
     }
 
+    const UploadFileDiv = () => {
+        return (
+            <div
+                className={`w-full h-full p-1 border-dashed border-2 border-stone-400 flex flex-col justify-center items-center space-y-6 ${dragging ? 'border-sky-600' : ''}`}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+            >
+                <div className="text-stone-500">
+                    <UploadIcon />
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <p className='text-stone-500 font-semibold select-none text-center text-sm lg:text-base'>Arraste e solte sua foto ou clique para selecionar</p>
+                    <p className='text-stone-400 font-medium select-none text-center text-xs'>(Apenas formatos JPEG ou JPG)</p>
+                </div>
+                <label htmlFor="fileInput"
+                >
+                    <p className="text-center text-sm lg:text-base mt-2 text-white font-semibold select-none cursor-pointer border border-stone-700 bg-stone-700 rounded px-5 py-1">Selecionar foto</p>
+                    <input
+                        id="fileInput"
+                        type="file"
+                        accept="*"
+                        onChange={handleFileInputChange}
+                        className="hidden"
+                    />
+                </label>
+                {errorMessage && <p className="text-red-500 font-medium flex text-sm items-center gap-2">{errorMessage}</p>}
+            </div>
+        )
+    }
+
     return (
         <Dialog open={true} onOpenChange={(open) => !open && onClose()}>
-            <DialogContent className="sm:max-w-[800px] h-[60%] flex flex-col">
+            <DialogContent className={`${uploadedPhoto === null ? 'sm:max-w-[800px] h-[60%]' : 'sm:max-w-[1200px] h-[90%]'} flex flex-col`}>
                 <DialogTitle>
                     Participar do Torneio
                 </DialogTitle>
-                <div
-                    className={`w-full h-full p-1 border-dashed border-2 border-stone-400 flex flex-col justify-center items-center space-y-6 ${dragging ? 'border-sky-600' : ''}`}
-                    onDragOver={handleDragOver}
-                    onDragLeave={handleDragLeave}
-                    onDrop={handleDrop}
-                >
-                    <div className="text-stone-500">
-                        <UploadIcon />
+                {uploadedPhoto && photoURL ? (
+                    <div className="w-full h-full flex flex-col items-center">
+                        <div className="relative flex justify-center items-center h-full w-full bg-stone-200">
+                            <Image
+                                src={photoURL}
+                                alt={"foto"}
+                                layout="fill"
+                                objectFit="contain"
+                                className="rounded-md"
+                            />
+                            <button
+                                className="absolute right-4 top-4 bg-black text-white flex justify-center items-center w-7 h-7 rounded-full"
+                                onClick={() => {
+                                    setUploadedPhoto(null);
+                                    setPhotoURL(null);
+                                }}
+                            ><CloseIcon /></button>
+                        </div>
+                        <div className="w-full pt-4">
+                            <label htmlFor="title" className="font-medium">Título da Foto</label>
+                            <input
+                                type="text"
+                                id="title"
+                                className="mt-1 p-2 border border-gray-300 rounded w-full"
+                                placeholder="(Opcional)"
+                                value={uploadedPhoto.title}
+                                onChange={(e) => handlePhotoTitleChange(e.target.value)}
+                            />
+                            <div className="mt-4">
+                                <label htmlFor="location" className="font-medium">Local da foto</label>
+                                <input
+                                    type="text"
+                                    id="location"
+                                    className="mt-1 p-2 border border-gray-300 rounded w-full"
+                                    placeholder="(Opcional)"
+                                    value={uploadedPhoto.location}
+                                    onChange={(e) => handlePhotoLocationChange(e.target.value)}
+                                />
+                            </div>
+                            <div className="w-full flex justify-end mt-10">
+                                <button
+                                    className="bg-black min-w-[250px] px-4 py-2 text-white rounded-md font-medium"
+                                >Registrar participação</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <p className='text-stone-500 font-semibold select-none text-center text-sm lg:text-base'>Arraste e solte sua foto ou clique para selecionar</p>
-                        <p className='text-stone-400 font-medium select-none text-center text-xs'>(Apenas formatos JPEG ou JPG)</p>
-                    </div>
-                    <label htmlFor="fileInput"
-                    >
-                        <p className="text-center text-sm lg:text-base mt-2 text-white font-semibold select-none cursor-pointer border border-stone-700 bg-stone-700 rounded px-5 py-1">Selecionar foto</p>
-                        <input
-                            id="fileInput"
-                            type="file"
-                            accept="*"
-                            onChange={handleFileInputChange}
-                            className="hidden"
-                        />
-                    </label>
-                    {errorMessage && <p className="text-red-500 font-medium flex text-sm items-center gap-2">{errorMessage}</p>}
-                </div>
+                ) : (
+                    <UploadFileDiv />
+                )}
             </DialogContent>
         </Dialog>
     )
--- client/src/components/icons/CloseIcon.tsx
+++ client/src/components/icons/CloseIcon.tsx
@@ -1,7 +1,5 @@
 export function CloseIcon({ size = 6 }: { size?: number }) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-${size}`}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x h-4 w-4"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg>
     )
 }
