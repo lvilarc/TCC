ac3014300ea49c353d841a4a0269099bc2cf67b9
Lucas Vilar <lucas.vilar.carneiro@gmail.com>
2025-02-23 23:01:16 -0300

    Model de votação

--- backend/src/tournaments/tournaments.controller.ts
+++ backend/src/tournaments/tournaments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UploadedFile, Req, Query, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UploadedFile, Req, Query, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { TournamentsService } from './tournaments.service';
 import { CreateTournamentDto } from './dto/create-tournament.dto';
 import { UpdateTournamentDto } from './dto/update-tournament.dto';
@@ -47,7 +47,58 @@ export class TournamentsController {
     return this.tournamentsService.findTournamentById(id);
   }
 
-  
+  // @UseGuards(JwtAuthGuard)
+  // @Get(':id/voting')
+  // async getVotingImages(
+  //   @Param('id') tournamentId: string,
+  //   @Query('mode') mode: string,
+  //   @User() user: UserSchema
+  // ) {
+  //   // Verifica se o torneio existe
+  //   const tournament = await this.tournamentsService.findTournamentById(tournamentId);
+  //   if (!tournament) {
+  //     throw new BadRequestException('Torneio não encontrado');
+  //   }
+
+  //   // Verifica se o usuário já votou nesse torneio
+  //   const userHasVoted = await this.tournamentsService.checkIfUserVoted(tournamentId, user.id);
+  //   if (userHasVoted) {
+  //     throw new BadRequestException('Você já participou da votação deste torneio');
+  //   }
+
+  //   // Busca todas as fotos do torneio
+  //   const photos = await this.tournamentsService.getTournamentPhotos(tournamentId);
+  //   if (!photos.length) {
+  //     throw new BadRequestException('Nenhuma foto disponível para votação');
+  //   }
+
+  //   // Seleciona as imagens com base no modo
+  //   let selectedPhotos = [];
+  //   const totalPhotos = photos.length;
+
+  //   switch (Number(mode)) {
+  //     case 1:
+  //       selectedPhotos = this.getRandomPhotos(photos, Math.min(10, totalPhotos));
+  //       break;
+  //     case 2:
+  //       selectedPhotos = this.getRandomPhotos(photos, 2);
+  //       break;
+  //     case 3:
+  //       selectedPhotos = [this.getRandomPhotos(photos, 1)[0]];
+  //       break;
+  //     case 4:
+  //       selectedPhotos = this.getRandomPhotos(photos, Math.min(40, totalPhotos));
+  //       break;
+  //     default:
+  //       throw new BadRequestException('Modo de votação inválido');
+  //   }
+
+  //   return {
+  //     tournamentId,
+  //     mode,
+  //     photos: selectedPhotos,
+  //   };
+  // }
 
   @Patch(':id')
   update(
